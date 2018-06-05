15f080f90e994f8e06a9d38995f0092107db3c59
Roni Bar-David <roni.bar-david@cellebrite.com>
2018-06-05 14:49:41 +0300

    times change

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const cool = require('cool-ascii-faces');
+
+let timesHandler = (req,res) => {
+  let result = '';
+  const times = process.env.TIMES || 7;
+  for (let i=0; i<times; i++){
+    result +=i + ' ';
+  }
+
+  res.send(result);
+}
+
+const myName = process.env.name || 'default';
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req,res)=>res.send(cool()))
-  .get('/roni', (req, res)=> res.send("hi roni"))
+  .get('/roni', (req, res)=> res.send("hi roni, name parameter is " + myName))
+  .get('/times',timesHandler)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  
